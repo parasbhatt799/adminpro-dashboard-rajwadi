cc6f963131811b535f0c8a9484e7da8af889eed0
jigsvanani <jigs.vanani@gmail.com>
2026-05-30 23:47:44 +0530

    feat: Add custom local logo for South Indian Bank Credit Card

--- src/components/user/UserBillPayment.tsx
+++ src/components/user/UserBillPayment.tsx
@@ -77,7 +77,7 @@ const getBankLogoUrl = (bankName: string) => {
   else if (name.includes("rbl")) domain = "rblbank.com";
   else if (name.includes("saraswat")) domain = "saraswatbank.com";
   else if (name.includes("sbm")) domain = "sbmbank.co.in";
-  else if (name.includes("south indian")) domain = "southindianbank.com";
+  else if (name.includes("south indian")) return "/south_indian_logo.png";
   else if (name.includes("suryoday")) domain = "suryodaybank.com";
   else if (name.includes("union bank")) domain = "unionbankofindia.co.in";
   else if (name.includes("yes bank")) domain = "yesbank.in";
